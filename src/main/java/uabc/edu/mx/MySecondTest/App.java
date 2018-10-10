d4dcbc989dced8af2e960aa4fe840377f6560623
puga <puga@imac-de-manuel-c-23.local>
2018-10-09 19:36:48 -0700

    Primera versión

---
+++ src/main/java/uabc/edu/mx/MySecondTest/App.java
@@ -0,0 +1,13 @@
+package uabc.edu.mx.MySecondTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
