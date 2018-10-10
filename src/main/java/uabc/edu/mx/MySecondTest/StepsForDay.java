8dd80de254f48a713783987c963bdb5f2def4355
puga <puga@imac-de-manuel-c-23.local>
2018-10-09 19:39:11 -0700

    Agregué JUnit 5

---
+++ src/main/java/uabc/edu/mx/MySecondTest/StepsForDay.java
@@ -0,0 +1,30 @@
+package uabc.edu.mx.MySecondTest;
+
+public class StepsForDay {
+    private int steps;
+    protected int goal;
+
+    public StepsForDay(int goal) {
+        this.goal = goal;
+        this.steps = 0;
+    }
+
+    public void inputSteps(int steps) {
+        this.steps = steps;
+    }
+
+    public void addSteps(int steps) {
+        this.steps += steps;
+    }
+
+    public void clearSteps(){
+        this.steps = 0;
+    }
+
+    public int get() { return steps; }
+
+    public boolean metGoal(){
+        return steps >= goal;
+    }
+
+}
